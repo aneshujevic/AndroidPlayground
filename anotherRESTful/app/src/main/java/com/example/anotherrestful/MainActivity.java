1b6399521e8eea0575cb88461da0cf198c06f86d
owlhoo <anes1996_h@hotmail.com>
2020-05-03 23:46:04 +0200

    first_commit

---
+++ anotherRESTful/app/src/main/java/com/example/anotherrestful/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.anotherrestful;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+    private String APIPath = "https://json-trial.000webhostapp.com/mobile/list/";
+    private ProgressDialog processDialog;
+    private JSONArray restfulJsonArray;
+    private int success = 0;
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.mobile_name_list);
+        new ServiceStubAsyncTask(this, this).execute();
+    }
+
+    private class ServiceStubAsyncTask extends AsyncTask<Void, Void, Void> {
+
+        private Context mContext;
+        private Activity mActivity;
+        String response = "";
+        HashMap<String, String> postDataParams;
+
+        public ServiceStubAsyncTask(Context context, Activity activity) {
+            mContext = context;
+            mActivity = activity;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            processDialog = new ProgressDialog(mContext);
+            processDialog.setMessage("Please  Wait ...");
+            processDialog.setCancelable(false);
+            processDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            postDataParams = new HashMap<String, String>();
+            postDataParams.put("HTTP_ACCEPT", "application/json");
+            HttpConnectionService service = new HttpConnectionService();
+            response = service.sendRequest(APIPath, postDataParams);
+            try {
+                success = 1;
+                JSONObject o = new JSONObject(response);
+                restfulJsonArray = o.getJSONArray("output");
+
+            } catch (JSONException e) {
+                success = 0;
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+            super.onPostExecute(result);
+
+            if (processDialog.isShowing()) {
+                processDialog.dismiss();
+            }
+
+            if (success == 1) {
+                if (null != restfulJsonArray) {
+                    ArrayAdapter listViewAdapter = new ArrayAdapter<String>(mContext, R.layout.mobile_name_listview);
+
+                    for (int i = 0; i < restfulJsonArray.length(); i++) {
+                        try {
+                            JSONObject jsonObject = restfulJsonArray.getJSONObject(i);
+                            listViewAdapter.add(jsonObject.get("name"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    listView.setAdapter(listViewAdapter);
+                }
+            }
+        }
+
+    }//end of async task
+}
